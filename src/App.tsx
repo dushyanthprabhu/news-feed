0d81f2127ab94ce67aa91c5f3d719414914ca4d5
Dushyanth Prabhu B <dushyanth.prabhu@niveussolutions.com>
2026-05-18 21:08:06 +0530

    bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
         return;
       });
     } else {
-      // newArticles = await fetchCountryNews();
+      newArticles = await fetchCountryNews();
     }
     dispatch(saveArticles(newArticles));
   };
